c8f62ee96099063ccf13e2213b3370d9b6a3c415
Muhammad Faizan <muhammadfaizanakx@gmail.com>
2024-12-19 12:39:10 +0500

    created pages home, about and contact (routing done), created components Navbar and Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <div>hello</div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/aontact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About />} />
+      </Routes>
     </>
   );
 }
