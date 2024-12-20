3b3bdad6fdeced609fd51f39224fc1f637d94576
Muhammad Faizan <muhammadfaizanakx@gmail.com>
2024-12-20 17:02:26 +0500

    created navbar for mobile with hamburger icon and buttons on mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-blue-800 text-white p-4 fixed bottom-0 w-full text-center shadow-lg">
+    <footer className="bg-gray-900 text-white p-4 fixed bottom-0 w-full text-center shadow-lg">
       <div className="footer-content">
-        <p>&copy; 2024 Dynamic LEO</p>
+        <p>&copy; 2024 Tejaria</p>
         <p>
           <a href="/about">About Us</a> | <a href="/contact">Contact</a>
         </p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { userState, useState } from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
 import logo1 from "../assests/logo1.png";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClose = () => {
+    setMenuOpen(false); // Close the menu
+  };
+
   return (
     <>
-      <nav className="bg-blue-800 p-5 flex shadow-lg ">
+      {/* Navbar Desktop Start */}
+
+      <nav className="bg-gray-900 p-5 flex shadow-lg ">
         <div>
           <Link to="/">
             <img src={logo1} alt="logo" className="h-10 w-auto " />
@@ -34,6 +41,83 @@ function Navbar() {
               Contact
             </Link>
             <button className="bg-white hover:bg-gray-200 p-2 rounded-sm font-bold">
+              <Link to="/contact">Sign up</Link>
+            </button>
+          </div>
+        </div>
+        {/* Navbar End Start */}
+
+        <button
+          className="block md:hidden"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <svg
+            className="w-6 h-6 text-white"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+
+        <div
+          className={`fixed top-0 left-0 h-full w-64 bg-gray-700 transform transition-transform duration-300 ${
+            menuOpen ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <button
+            className="absolute  text-gray-400"
+            onClick={() => setMenuOpen(false)}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          </button>
+
+          <div className="flex flex-col items-center mt-10 space-y-4">
+            <Link
+              to="/"
+              className="text-white hover:bg-gray-700 p-2 rounded-md"
+              onClick={handleMenuClose}
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className="text-white hover:bg-gray-700 p-2 rounded-md"
+              onClick={handleMenuClose}
+            >
+              About
+            </Link>
+            <Link
+              to="/contact"
+              className="text-white hover:bg-gray-700 p-2 rounded-md"
+              onClick={handleMenuClose}
+            >
+              Contact
+            </Link>
+            <button
+              className="bg-white hover:bg-gray-200 p-2 rounded-sm font-bold"
+              onClick={handleMenuClose}
+            >
               <Link to="/contact">Sign up</Link>
             </button>
           </div>
